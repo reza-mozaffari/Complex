5305c2d1c63e35f1d92a7c02bdd737a88d95e7c0
reza-mozaffari <reza.mozaffari@gmail.com>
2022-08-01 11:26:41 +0430

    Add files via upload

---
+++ Complex.py
@@ -0,0 +1,66 @@
+"""
+	Complex.py
+	
+	M. Reza Mozaffari
+	Physics Group, University of Qom
+"""
+
+from math import *
+
+class Complex ():
+	def __init__ (self, x=0.0, y=0.0):
+		self.x = float(x)
+		self.y = float(y)
+	
+	def __str__ (self):
+		if self.y < 0:
+			return "{:.4f}-{:.4f}j".format(self.x, abs(self.y))
+		else:
+			return "{:.4f}+{:.4f}j".format(self.x, self.y)
+			
+	def __add__ (self, other):
+		return Complex (self.x + other.x, self.y + other.y)
+		
+	def __sub__ (self, other):
+		return Complex (self.x - other.x, self.y - other.y)	
+	
+	def __mul__ (self, other):
+		if type(other) == type(self):
+			return 	Complex (self.x*other.x - self.y*other.y, self.x*other.y + self.y*other.x)
+		else:
+			return 	Complex (self.x*other, self.y*other)
+
+	def __rmul__ (self, scalar):
+		return 	Complex (self.x*scalar, self.y*scalar)
+
+	def conj (self):
+		return Complex (self.x,-self.y)
+	
+	def __truediv__ (self, other):
+		if type(other) == type(self):
+			r2 = other.x*other.x + other.y*other.y
+			C = self.__mul__ (other.conj())
+			return Complex (C.x/r2, C.y/r2)
+		else:
+			return Complex (self.x/other, self.y/other)
+
+	def __abs__ (self): 
+		return 	sqrt(self.x*self.x + self.y*self.y)
+
+	def radius (self): 
+		return 	self.__abs__()
+	
+	def angle (self):
+		return atan2(self.y,self.x)
+	
+	def __pow__ (self, other):
+		if type(self) == type(other):
+			C1 = Complex(log(self.radius()), self.angle())
+			C2 = other.__mul__(C1)
+			x2, y2  = C2.x, C2.y
+			return Complex (exp(x2)*cos(y2), exp(x2)*sin(y2))
+		else:
+			r = self.radius()
+			theta = self.angle()
+			return Complex (r**other * cos(theta*other), r**other * sin(theta*other))
+
---
+++ main.py
@@ -0,0 +1,50 @@
+"""
+	main.py
+	
+	M. Reza Mozaffari
+	Physics Group, University of Qom
+"""
+
+from Complex import *
+
+if __name__ == "__main__":
+
+	C0 = Complex()
+	print("C0 = ", C0)
+	
+	C1 = Complex(3.0, 3.0)
+	print("C1 = ", C1)
+
+	C0 = C1
+	print("C0 = ", C0)
+	
+	C2 = Complex(1.0,-2.0)
+	print("C2 = ", C2)
+
+	print("C1 + C2 = ", C1 + C2)
+	
+	print("C1 - C2 = ", C1 - C2)
+	
+	print("C1 * C2 = ", C1 * C2)
+	
+	print("2.0 * C2 = ", 2.0 * C2)
+	print("C2 * 2.0 = ", C2 * 2.0)
+
+	print("C2 / C1 = ", C2 / C1)
+	print("3.0 * C2 / 2.0 = ", 3.0 * C2 / 2.0)
+	
+	print("conj(C1) = ", C1.conj())
+	print("C1 * conj(C1) = ", C1 * C1.conj())
+	
+	print("|C1| = ", abs(C1))
+	print("C1 radius = ", C1.radius())
+	print("C1 angle = ", C1.angle(), "rad.")
+	print("C1 angle = ", C1.angle()*180.0/pi, "deg.")
+	
+	C3 = Complex(2.0, 3.0)
+	print("C3 = ", C3)
+	print("C3**1.5 = ", C3**1.5)
+	
+	C4 = Complex(1.0, 1.0)
+	print("C4 = ", C4)
+	print("C3**C4 = ", C3**C4)
